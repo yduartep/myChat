f0af368d0c6fd83bf4d1259cc1c57605380a0a32
Yahima Duarte <yahima.duarte@iristrace.com>
2021-02-09 13:07:10 +0100

    add typing event

--- public/js/app.js
+++ public/js/app.js
@@ -1,72 +1,105 @@
 'use strict';
 
 window.onload = () => {
-  // page loaded
-  let nameAdded = false;
-  const socket = io();
+    // page loaded
+    let nameAdded = false;
+    const socket = io();
 
-  socket.on('connected', () => {
-    nameAdded = false;
-    var ul = document.getElementById('messages');
-    ul.innerHTML = '';
-  });
+    socket.on('connected', () => {
+        nameAdded = false;
+        var ul = document.getElementById('messages');
+        ul.innerHTML = '';
+    });
 
-  socket.on('new-message', (data) => {
-    addChatMessage(data);
-  });
+    socket.on('new-message', (data) => {
+        addChatMessage(data);
+    });
 
-   // Whenever the server emits 'typing', show the typing message
-   socket.on('typing', (data) => {
-    addChatTyping(data);
-  });
+    // Whenever the server emits 'typing', show the typing message
+    socket.on('typing', (data) => {
+        console.log(`User ${data.username} ${data.message}.`);
+        addChatTyping(data);
+    });
 
-  socket.on('stop-typing', (data) => {
-    removeChatTyping(data);
-  });
-  // Whenever the server emits 'user joined', log it in the chat body
-  socket.on('user-joined', (data) => {
-    alert(`${data.username} joined with color: ${data.color}. Total: ${data.numUsers}`);
-  });
+    socket.on('stop-typing', (data) => {
+        console.log('The user has stopped typing');
+        removeChatTyping(data);
+    });
+    // Whenever the server emits 'user joined', log it in the chat body
+    socket.on('user-joined', (data) => {
+        alert(`${data.username} joined with color: ${data.color}. Total: ${data.numUsers}`);
+    });
 
-  // Whenever the server emits 'user left', log it in the chat body
-  socket.on('user-left', (data) => {
-    alert(`${data.username} left. Total: ${data.numUsers}`);
-  });
+    // Whenever the server emits 'user left', log it in the chat body
+    socket.on('user-left', (data) => {
+        alert(`${data.username} left. Total: ${data.numUsers}`);
+    });
 
-const btnLogin = document.getElementById('btn-login');
-btnLogin.addEventListener('click', function(e) {
-    const chatContainer = document.getElementById('chat');
-    const usernameInput = document.getElementById('usernameInput');
-    const colorInput = document.getElementById('colorInput');
-    if(usernameInput && usernameInput.value && colorInput && colorInput.value){
-      nameAdded = true;
-      chatContainer.style.visibility = 'visible';
-      socket.emit('add-user', {username: usernameInput.value, color: colorInput.value});
+    // DOM EVENTS
+    const inputMsg = document.getElementById('msg');
+    inputMsg.addEventListener('focus', function(e) {
+        socket.emit('typing');
+    });
+    inputMsg.addEventListener('blur', function(e) {
+        socket.emit('stop-typing');
+    });
+
+    const btnLogin = document.getElementById('btn-login');
+    btnLogin.addEventListener('click', function(e) {
+        const chatContainer = document.getElementById('chat');
+        const usernameInput = document.getElementById('usernameInput');
+        const colorInput = document.getElementById('colorInput');
+        if (usernameInput && usernameInput.value && colorInput && colorInput.value) {
+            nameAdded = true;
+            chatContainer.style.visibility = 'visible';
+            socket.emit('add-user', {
+                username: usernameInput.value,
+                color: colorInput.value
+            });
+        }
+    });
+
+    const btnSendMsg = document.getElementById('btn-send');
+    btnSendMsg.addEventListener('click', function(e) {
+        const msgInput = document.getElementById('msg');
+        let message = msgInput.value;
+        // if there is a non-empty message and a socket connection
+        if (message && nameAdded) {
+            msgInput.value = '';
+            addChatMessage({
+                username: document.getElementById('usernameInput').value,
+                color: document.getElementById('colorInput').value,
+                message
+            });
+            // tell server to execute 'new message' and send along one parameter
+            socket.emit('new-message', message);
+        }
+    });
+
+    function addChatMessage(data) {
+        var ul = document.getElementById('messages');
+        var li = document.createElement('li');
+        li.style.color = data.color;
+        li.appendChild(document.createTextNode(data.message));
+        ul.appendChild(li);
     }
-});
 
-const btnSendMsg = document.getElementById('btn-send');
-btnSendMsg.addEventListener('click', function(e) {
-  const msgInput = document.getElementById('msg');
-    let message = msgInput.value;
-    // if there is a non-empty message and a socket connection
-    if (message && nameAdded) {
-      msgInput.value = '';
-      addChatMessage({ 
-        username: document.getElementById('usernameInput').value, 
-        color: document.getElementById('colorInput').value,
-        message 
-      });
-      // tell server to execute 'new message' and send along one parameter
-      socket.emit('new-message', message);
+    function addChatTyping(data) {
+        var ul = document.getElementById('messages');
+        var li = document.createElement('li');
+        li.setAttribute('class', 'typing');
+        li.setAttribute('user', data.username);
+        li.appendChild(document.createTextNode(data.message));
+        ul.appendChild(li);
     }
-});
 
-function addChatMessage(data) {
-    var ul = document.getElementById('messages');
-    var li = document.createElement('li');
-    li.style.color = data.color;
-    li.appendChild(document.createTextNode(data.message));
-    ul.appendChild(li);
-  }
+    function removeChatTyping(data) {
+        var ul = document.getElementById('messages');
+        var typings = document.getElementsByClassName('typing');
+        for (let t in typings) {
+            if (t.user === data.username) {
+                ul.removeChild(typing);
+            }
+        }
+    }
 }
--- server.js
+++ server.js
@@ -19,52 +19,64 @@ io.on('connection', (socket) => {
 
     console.log('A user is connected');
     socket.broadcast.emit('connected');
-    
+
     // when the user disconnects.. perform this
     socket.on('disconnect', () => {
-      console.log('A user is disconnected');
-    if (addedUser) {
-      --numUsers;
+        console.log('A user is disconnected');
+        if (addedUser) {
+            --numUsers;
 
-      // echo globally that this client has left
-      socket.broadcast.emit('user-left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
-  });
+            // echo globally that this client has left
+            socket.broadcast.emit('user-left', {
+                username: socket.username,
+                color: socket.color,
+                numUsers: numUsers
+            });
+        }
+    });
 
     // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', () => {
-    socket.broadcast.emit('typing', {username: socket.username});
-  });
-
-  // when the client emits 'stop-typing', we broadcast it to others
-  socket.on('stop-typing', () => {
-    socket.broadcast.emit('stop-typing', {username: socket.username});
-  });
-
-  socket.on('new-message', (message) => {
-    console.log('New message received: ' + message);
-    socket.broadcast.emit('new-message', {username: socket.username,message: data, color: socket.color});
-  });
-
-  socket.on('add-user', (data) => {
-      console.log('New User is joined: ' + data.username + ' with color: '+ data.color);
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = data.username;
-    socket.color = data.color;
-    ++numUsers;
-    addedUser = true;
-    
-    console.log('Total Users connected: ' + numUsers);
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user-joined', {
-      username: socket.username,
-      color: socket.color,
-      numUsers: numUsers
+    socket.on('typing', () => {
+        console.log('The user ' + socket.username + ' is typing ...');
+        socket.broadcast.emit('typing', {
+            username: socket.username,
+            message: 'is typing...'
+        });
     });
-  });
+
+    // when the client emits 'stop-typing', we broadcast it to others
+    socket.on('stop-typing', () => {
+        console.log('The user ' + socket.username + ' stop typing ...');
+        socket.broadcast.emit('stop-typing', {
+            username: socket.username
+        });
+    });
+
+    socket.on('new-message', (message) => {
+        console.log('New message received: ' + message);
+        socket.broadcast.emit('new-message', {
+            username: socket.username,
+            message,
+            color: socket.color
+        });
+    });
+
+    socket.on('add-user', (data) => {
+        console.log('New User is joined: ' + data.username + ' with color: ' + data.color);
+        if (addedUser) return;
+
+        // we store the username in the socket session for this client
+        socket.username = data.username;
+        socket.color = data.color;
+        ++numUsers;
+        addedUser = true;
+
+        console.log('Total Users connected: ' + numUsers);
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user-joined', {
+            username: socket.username,
+            color: socket.color,
+            numUsers: numUsers
+        });
+    });
 });
